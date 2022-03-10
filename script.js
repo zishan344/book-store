ae17f54a76078b2686ca04f4a0246e2395cf1276
zishan344 <zishanahmed344@gmail.com>
2022-03-10 16:57:06 +0600

    done the overflow writing and undefined

--- script.js
+++ script.js
@@ -91,8 +91,12 @@ const createCard = (book) => {
   const div = document.createElement("div");
   div.classList.add("card");
 
-  let overview = book.overview;
-  console.log(book);
+  let overview;
+  if (book.overview.length > 100) {
+    overview = book.overview.slice(0, 100) + "...";
+  } else {
+    overview = book.overview;
+  }
   div.innerHTML = `
   <div class="image-container">
     <img
